8a86867fd13aa5f9584dbbe0c245299da2ec7a4d
moussa-sefin <moussa.sefin@gmail.com>
2023-06-04 11:45:03 +0300

    favolite character

--- toolbox/webtool/urls.py
+++ toolbox/webtool/urls.py
@@ -1,9 +1,11 @@
 from django.urls import  path
-from .views import home, tool_details           
+from .views import home, tool_details, AddToFavolite           
 
 
 urlpatterns = [
 
     path("", home, name='home'),
     path('tool-detail/<int:pk>', tool_details, name='tool_detail'),
+    path('add-to-favolite/<int:pk>', AddToFavolite.as_view(), name='AddToFavolite')
+
 ]
--- toolbox/webtool/views.py
+++ toolbox/webtool/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .models import Tool, Favorite, Category, Review
+from django.views.generic import View
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -20,3 +23,13 @@ def tool_details(request, pk):
     reviews = Review.objects.filter(user=user, tool=tool)
     ctx = {'tool': tool, "favorites":favorites, "categories":categories, "reviews":reviews}
     return render(request, 'webtool/toolDetail.html', ctx)
+
+class AddToFavolite(View):
+    def get(self, request, pk):
+        success_url = reverse_lazy('home')
+        user = request.user
+        tool = Tool.objects.get(id=pk)
+        favolite = Favorite.objects.create(user=user, tool=tool)
+        favolite.save()
+        print("added")
+        return redirect(success_url)
